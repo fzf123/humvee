4c60f622ea117b20dfd4d58bf288cfe0d63e6ed4
fzf123 <fangzhaofa@gmail.com>
2019-10-02 23:11:00 +0800

    up

--- queue/sync_queue.hpp
+++ queue/sync_queue.hpp
@@ -1,26 +1,53 @@
-#include "queue.h"
+#pragma once
+
+#include <pthread.h>
+
+#include <atomic>
+#include <queue>
 
 namespace humvee {
 
-template<typename T>
-Queue<T>::Queue() {
+template<class T>
+class SyncQueue {
+public:
+    SyncQueue();
+    explicit SyncQueue(int max_size);
+    ~SyncQueue();
+
+    // default blocking queue
+    bool push(const T& obj, bool try_push = false);
+    bool pop(T& obj, bool try_pop = false);
+    bool empty() const;
+private:
+    void init(int max_size);
+
+    pthread_cond_t _q_no_empty;
+    pthread_cond_t _q_no_full;
+    pthread_mutex_t _qlock;
+    std::queue<T> _queue;
+    std::atomic_int _queue_size;
+    int _max_size;
+};
+
+template<class T>
+SyncQueue<T>::SyncQueue() {
     init(0);
 }
 
-template<typename T>
-Queue<T>::Queue(int max_size) {
+template<class T>
+SyncQueue<T>::SyncQueue(int max_size) {
     init(max_size);
 }
 
-template<typename T>
-Queue<T>::~Queue() {
+template<class T>
+SyncQueue<T>::~SyncQueue() {
     pthread_cond_destroy(&_q_no_empty);
     pthread_cond_destroy(&_q_no_full);
     pthread_mutex_destroy(&_qlock);
 }
 
-template<typename T>
-void Queue<T>::init(int max_size) {
+template<class T>
+void SyncQueue<T>::init(int max_size) {
     _max_size = max_size;
     _queue_size.store(0);
     pthread_cond_init(&_q_no_empty, NULL);
@@ -28,8 +55,13 @@ void Queue<T>::init(int max_size) {
     pthread_mutex_init(&_qlock, NULL);
 }
 
-template<typename T>
-bool Queue<T>::push(const T& obj, bool try_push) {
+template<class T>
+bool SyncQueue<T>::empty() const {
+    return _queue_size.load() <= 0;
+}
+
+template<class T>
+bool SyncQueue<T>::push(const T& obj, bool try_push) {
     pthread_mutex_lock(&_qlock);
     if (_max_size > 0) {
         // if _max_size <= 0, then queue size is unlimit
@@ -48,8 +80,8 @@ bool Queue<T>::push(const T& obj, bool try_push) {
     return true;
 }
 
-template<typename T>
-bool Queue<T>::pop(T& obj, bool try_pop) {
+template<class T>
+bool SyncQueue<T>::pop(T& obj, bool try_pop) {
     pthread_mutex_lock(&_qlock);
     while (_queue_size.load() <= 0) {
         if (try_pop) {
@@ -66,4 +98,4 @@ bool Queue<T>::pop(T& obj, bool try_pop) {
     return true;
 }
 
-}
+} // end of namespace
